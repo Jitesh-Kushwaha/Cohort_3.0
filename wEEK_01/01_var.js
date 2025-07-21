075ab1a0bd86d90c59dce7937a294e76e28b67a0
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:56:19 +0530

    Add files via upload

---
+++ wEEK_01/01_var.js
@@ -0,0 +1,14 @@
+let name = "Jitesh";
+const age = 20;
+var isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+var Student = true;
+Student = "Jitesh";
+Student = 22;
+Student = "Kushwaha";
+
+console.log(Student);
