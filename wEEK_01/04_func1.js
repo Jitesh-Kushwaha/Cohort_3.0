84652d0e685b7d761c78755eac31337796b47512
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:58:28 +0530

    Add files via upload

---
+++ wEEK_01/04_func1.js
@@ -0,0 +1,12 @@
+//Function declaration
+function greet(name) {
+  return "Hello, " + name;
+}
+
+let message = greet("John"); //Hello John
+let message1 = greet("Jay"); //Hello Jay
+let message2 = greet("Atharva"); //Hello Atharva
+
+console.log(message);
+console.log(message1);
+console.log(message2);
