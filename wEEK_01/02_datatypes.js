ccafb991fa1fcfd5a333022c01d6c654b7e0d6ae
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:57:15 +0530

    Add files via upload

---
+++ wEEK_01/02_datatypes.js
@@ -0,0 +1,16 @@
+let numb = 04; //Number
+let string = "Myself Jitesh"; //String
+let question = true; //Boolean
+let arr = [1, 2, 3, 4]; //Arrays
+
+console.log(numb);
+console.log(string);
+console.log(question);
+console.log(arr);
+
+let users = ["Jitesh", "Harkirat", "Atharva", "Jay"];
+console.log(users);
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[6]);
