acd25d2537da3973286bec65607ff53285d95fdc
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:59:19 +0530

    Add files via upload

---
+++ wEEK_01/07_advobj.js
@@ -0,0 +1,17 @@
+let user1name = "Jitesh";
+let user1age = 20;
+let user1gender = "male";
+
+//INSTEAD we also can write it as;
+
+let user1 = {
+  name: "Jitesh",
+  age: 20,
+  gender: "male",
+};
+
+console.log(user1);
+console.log(user1.name);
+console.log(user1["name"]);
+console.log(user1.age);
+console.log(user1.gender);
