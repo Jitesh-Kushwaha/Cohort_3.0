bede8140e26cb19cd9344907e5f2836a318fcc01
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:59:02 +0530

    Add files via upload

---
+++ wEEK_01/06_loops.js
@@ -0,0 +1,19 @@
+let users = ["Jitesh", "Harkirat", "Atharva", "Jay"];
+
+// //initiation section
+// //condition section
+// //incrementation section
+
+for (let i = 0; i < 4; i++) {
+  console.log(users[i]);
+}
+
+//further more modifying the code
+//more of a dynamic code
+
+let users11 = ["Amar", "Akbar", "Anthony", "Jiru", "Viru"];
+
+let totalusers = users11.length;
+for (let i = 0; i < totalusers; i++) {
+  console.log(users11[i]);
+}
