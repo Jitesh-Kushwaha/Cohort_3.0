40071ac2dbfacef5322bb69268802ceabe1645c0
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-21 22:58:09 +0530

    Add files via upload

---
+++ wEEK_01/03_Operators.js
@@ -0,0 +1,9 @@
+let sum = 10 + 5; //Arithmetic operation
+let isEqual = 10 == 10; //Comparison operator
+let isEqual2 = 10 == 8;
+let isTrue = true && false; //Logical Operator
+
+console.log(sum);
+console.log(isEqual);
+console.log(isEqual2);
+console.log(isTrue);
